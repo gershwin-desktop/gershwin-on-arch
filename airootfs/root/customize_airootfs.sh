3a4941f1da09b2bb1e2ac2bb12e3920f61cf3acb
probonopd <probonopd@users.noreply.github.com>
2025-12-15 02:20:27 +0100

    Remove gershwin-components build steps
    This is now handled by gershwin-build.
    Reference: https://github.com/gershwin-desktop/gershwin-on-debian/pull/3

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -25,21 +25,6 @@ cd /root
 
 . /System/Library/Makefiles/GNUstep.sh
 
-# TODO: Move to gershwin-build
-git clone --recursive https://github.com/probonopd/gershwin-components && cd gershwin-components
-sudo pacman -Syu dbus pam pkgconf --noconfirm
-cp -r /usr/lib/dbus-1.0/include/dbus /usr/include/ || true # Arch Linux; FIXME: Find a better way
-cp -r /usr/lib/*-linux-gnu/dbus-1.0/include/dbus /usr/include/ || true # Debian; FIXME: Find a better way
-find /usr/include -name "dbus-arch-deps.h"
-cd Menu
-make -j$(nproc)
-sudo -E make install
-cd -
-cd LoginWindow
-make -j$(nproc)
-sudo -E make install
-cd -
-
 # Everything that comes with the System should be in /System
 mkdir -p /System/Applications /System/Library/Tools
 sudo mv /Local/Applications/* /System/Applications || true
